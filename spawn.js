709044a1b3879cf44a0384020c1d7a93c1b9bebc
Tim Smith <code@abznak.com>
2017-01-29 15:21:39 +1000

    added help and tidying spawn spell

--- spawn.js
+++ spawn.js
@@ -1,8 +1,19 @@
 var magik = magikcraft.io;
 
+
+
 function spawn(typ, n, times, baseloc){
-    n = parseInt(n) || 20;
-    times = parseInt(times) || 1;
+    sender = server.getPlayer(sender.getName());
+	if (typ == '?') {
+		magik.dixit("/spawn <typ> <n> <times>");
+		magik.dixit("typ - ZOMBIE or CREEPER etc, dot (.) for a mix");
+		magik.dixit("n - number to spawn per wave");
+		magik.dixit("times - number of waves");
+		magik.dixit("for example, try: /cast spawn . 5 30");
+		return;
+	}
+    n = parseInt(n) || 5;
+    times = parseInt(times) || 5;
     var sender2 = sender;
     if (typ == '.') {
         typ = undefined;
@@ -10,44 +21,30 @@ function spawn(typ, n, times, baseloc){
     if (!baseloc) {
         baseloc = create_spawn_point(magik.aspecto());
     }
-        
 
-    //var arrow = player.getWorld().spawnEntity(sender.location, org.bukkit.entity.EntityType.ZOMBIE);
-    
     for (var i = 0; i < n; i++) {
         var loc = rndBlock(baseloc.getBlock().getRelative(0, 0, 0).location, 4,0).location;
-        //var loc = magik.aspecto().getBlock().getRelative(0, 2, 0).location;
-        //TODO: set pitch and yaw, try with fireball again
-        //var arrow = player.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType.DRAGON_FIREBALL);
         var typ2 = typ || rnd_monster_type();
-        var arrow = loc.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType[typ2]); 
-//        arrow.setShooter(sender);
+        var mob = loc.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType[typ2]); 
         var v = sender.getEyeLocation().getDirection().multiply(2);
         
         var dir = Math.PI*2*Math.random();
-        var scale = 0.25;
+        var scale = 0.45;
         v.setX(scale*Math.sin(dir));
-        v.setY(scale*4);
+        v.setY(scale*2);
         v.setZ(scale*Math.cos(dir));
-        //magik.dixit(loc);
-        arrow.setVelocity(v);
-        //arrow.setDirection(v);
-        //arrow.setMetadata("ArrowType", new MyMetadata(this, "stick"));    
-        //magik.dixit(arrow);
+        mob.setVelocity(v);
     }
     
     times -= 1;
     if (times > 0) {
-        //magik.dixit(plugin);
         var task = magik.setTimeout(
             function() {
                 sender = sender2;
                 spawn(typ, n, times, baseloc);
-                //magik.dixit("timeout!")
             }, 
             200
         );
-        //magik.dixit(task);
     }
         
 }
@@ -56,16 +53,24 @@ function create_spawn_point(loc) {
     var block = loc.getBlock();
     var r = 1;
     var depth = 3;
-    for (x = -r; x <= r; x++) {
-        for (y = -r; y <= r; y++) {
-            for (z = -depth; z <= 0; z++) {
-                block.getRelative(x,z,y).setType(org.bukkit.Material['AIR']);
+
+    var spawnloc = block.getRelative(0, -depth, 0).getLocation();
+
+
+    
+    //explicitly clear a space
+    for (y = -depth; y <= 0; y++) {
+        var rr = Math.floor(r);  //for changing the shape of the hole.  I liked no slope, in the end
+        for (x = -rr; x <= rr; x++) {
+            for (z = -rr; z <= rr; z++) {
+                block.getRelative(x,y,z).setType(org.bukkit.Material['AIR']);
             }
         }
     }
     
-    var spawnloc = block.getRelative(0, -depth, 0).getLocation();
+    //explode that space, for effect and to mess up the shape a little
     spawnloc.getWorld().createExplosion(spawnloc,3);
+    
     return spawnloc;
 }
 
@@ -119,4 +124,5 @@ function scale(r, from, to) {
 function rndBlock(loc, radius, height) {
     return loc.getBlock().getRelative(rnorm(0, radius/5), runif(0, height), rnorm(0, radius/5));
 }
+
 
