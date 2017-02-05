5d406b73946bb154920ac584f9ea35977f6c03de
Tim Smith <code@abznak.com>
2017-02-05 22:15:47 +1000

    tidying

--- l.js
+++ l.js
@@ -1,24 +1,21 @@
 var magik = magikcraft.io;
 
 // explode  
-function l(n, repeats, delay) {
+function l(repeats, delay) {
     var sender2 = sender;
-    n = parseInt(n) || 30;
     repeats = parseInt(repeats) || 1;
     delay = parseInt(delay) || 200;
-    //sender.location.getWorld().createExplosion(magik.aspecto(),n);
     magik.fulmen();        
     repeats--;
-    //magik.dixit(sender.isSneaking() ? "yes" : "no");
     if (sender.isSneaking()) {
-        magik.dixit("e done via sneak");
+        magik.dixit("l done via sneak");
         return;
     }
     if (repeats > 0) {
         var task = magik.setTimeout(
             function() {
                 sender = sender2;
-                l(n, repeats, delay);
+                l(repeats, delay);
             },
             delay
         );
