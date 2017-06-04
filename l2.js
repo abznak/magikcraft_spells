7a4889c9063f062fe263027dc861c971ad7cbc80
Joshua Wulf <sitapati@worldsankirtan.net>
2017-06-04 16:42:37 +1000

    Update l2.js
    Uses the new Magikcraft Open Source API. Works on au.open.magikcraft.io.

--- l2.js
+++ l2.js
@@ -1,28 +1,22 @@
-var magik = magikcraft.io;
+const magik = magikcraft.io;
 
 // lightning  
 // lpc - lightning per circle
 // cc - circle count
-function l2(lpc, cc, i, delay) {
-    lpc = parseInt(lpc) || 10;
-    cc = parseInt(cc) || 3;
-    i = parseInt(i) || 0;
-    m = parseInt(m) || 30;
-    delay = parseInt(delay) || 100;
+function l2(lpc = 10, cc = 3, i = 0, delay = 100) {
+    const m = 30;
+    const sender = magik.getSender();
+    magik.dixit("l2 - "+ m + " " + i + " " + delay);
 
-    magik.dixit("m2 - "+ m + " " + i + " " + delay);
-
-
-    var oldloc = magik.hic();
-    var target = magik.hic();
+    const oldloc = magik.hic();
+    const target = magik.hic();
     i++;     
     for (var k = 0; k < m; k++) {
-        target.yaw = 360/m*k;
-        target.pitch = 90 - i * 5;
+        target.setYaw(360/m*k);
+        target.setPitch(90 - i * 5);
         magik.ianuae(target);  
-        magik.fulmen();        
+        magik.shakti();        
     }
-    
 
     if (sender.isSneaking()) {
         magik.dixit("l2 done via sneak");
@@ -30,14 +24,13 @@ function l2(lpc, cc, i, delay) {
     }
     if (i < cc) {
         var task = magik.setTimeout(
-            function() { 
-                magik.dixit(sender);              
+            () => {             
                 l2(lpc, cc, i, delay);
             },
             delay
         );
     } else {
-        magik.dixit("e done");
+        magik.dixit("l2 done");
     }
 }
 
