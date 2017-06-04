0122176b40d5179bb3871bdb5f6014b6e1aa0258
Joshua Wulf <sitapati@worldsankirtan.net>
2017-06-04 16:56:53 +1000

    Update spawn.js
    Uses the new Magikcraft Open Source API. Works on au.open.magikcraft.io.

--- spawn.js
+++ spawn.js
@@ -1,9 +1,8 @@
 var magik = magikcraft.io;
 
-
-
-function spawn(typ, n, times, baseloc){
-    sender = server.getPlayer(sender.getName());
+function spawn(typ, n = 5, times = 5, baseloc){
+    const EntityType = magik.type("entity.EntityType");
+    const sender = magik.getSender();
 	if (typ == '?') {
 		magik.dixit("/cast spawn <typ> <n> <times>");
 		magik.dixit("typ - ZOMBIE or CREEPER etc, dot (.) for a mix");
@@ -12,9 +11,6 @@ function spawn(typ, n, times, baseloc){
 		magik.dixit("for example, try: /cast spawn . 5 30");
 		return;
 	}
-    n = parseInt(n) || 5;
-    times = parseInt(times) || 5;
-    var sender2 = sender;
     if (typ == '.') {
         typ = undefined;
     }
@@ -22,14 +18,14 @@ function spawn(typ, n, times, baseloc){
         baseloc = create_spawn_point(magik.aspecto());
     }
 
-    for (var i = 0; i < n; i++) {
-        var loc = rndBlock(baseloc.getBlock().getRelative(0, 0, 0).location, 4,0).location;
-        var typ2 = typ || rnd_monster_type();
-        var mob = loc.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType[typ2]); 
-        var v = sender.getEyeLocation().getDirection().multiply(2);
+    for (let i = 0; i < n; i++) {
+        const loc = rndBlock(baseloc.getBlock().getRelative(0, 0, 0).location, 4,0).location;
+        const typ2 = typ || rnd_monster_type();
+        const mob = loc.getWorld().spawnEntity(loc, EntityType[typ2]); 
+        const v = sender.getEyeLocation().getDirection().multiply(2);
         
-        var dir = Math.PI*2*Math.random();
-        var scale = 0.45;
+        const dir = Math.PI*2*Math.random();
+        const scale = 0.45;
         v.setX(scale*Math.sin(dir));
         v.setY(scale*2);
         v.setZ(scale*Math.cos(dir));
@@ -38,9 +34,8 @@ function spawn(typ, n, times, baseloc){
     
     times -= 1;
     if (times > 0) {
-        var task = magik.setTimeout(
-            function() {
-                sender = sender2;
+        const task = magik.setTimeout(
+            () => {
                 spawn(typ, n, times, baseloc);
             }, 
             200
@@ -50,20 +45,19 @@ function spawn(typ, n, times, baseloc){
 }
 
 function create_spawn_point(loc) {
-    var block = loc.getBlock();
-    var r = 1;
-    var depth = 3;
+    const block = loc.getBlock();
+    const r = 1;
+    const depth = 3;
+    const Material = magik.type("Material");
 
-    var spawnloc = block.getRelative(0, -depth, 0).getLocation();
+    const spawnloc = block.getRelative(0, -depth, 0).getLocation();
 
-
-    
     //explicitly clear a space
-    for (y = -depth; y <= 0; y++) {
+    for (let y = -depth; y <= 0; y++) {
         var rr = Math.floor(r);  //for changing the shape of the hole.  I liked no slope, in the end
-        for (x = -rr; x <= rr; x++) {
-            for (z = -rr; z <= rr; z++) {
-                block.getRelative(x,y,z).setType(org.bukkit.Material['AIR']);
+        for (let x = -rr; x <= rr; x++) {
+            for (let z = -rr; z <= rr; z++) {
+                block.getRelative(x,y,z).setType(Material['AIR']);
             }
         }
     }
@@ -76,7 +70,7 @@ function create_spawn_point(loc) {
 
 
 function rnd_monster_type() {
-    var types = [
+    const types = [
         'ZOMBIE',
         'ZOMBIE',
         'ZOMBIE',
@@ -97,17 +91,15 @@ function rnd_from_array(arry) {
 
 }
 
-
 function randn_bm() {
-    var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
-    var v = 1 - Math.random();
+    const u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+    const v = 1 - Math.random();
     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
 }
 
-
 function rnorm_unit(count) {
-    var ret = 0;
-    for (var i = 0; i < count; i++) {
+    let ret = 0;
+    for (let i = 0; i < count; i++) {
         ret += Math.random();
     }
     return ret/count;
